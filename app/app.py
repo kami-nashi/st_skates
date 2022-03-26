d9e7cda9057c6defe1b97b3948fd8f34ad187842
Ashley Young <asyoung@redhat.com>
2022-03-26 18:27:55 -0400

    updating skaterListSkates to now show how many hours per skate config

--- app/app.py
+++ app/app.py
@@ -1,42 +1,52 @@
-from flask import Flask
-
-from flask import request, jsonify
-import json
-import configparser as conf
+from flask import Flask, request, jsonify
+import st_dbConf
 import lib.baseLogic as base
 
 app = Flask(__name__)
-appConfig = base.baseConfig()
+appConfig = st_dbConf.baseConfig()
 app.config['SECRET_KEY'] = appConfig
+
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Your destination is not here ... </p>"
+
 
 @app.route('/api/v1/resources/skates/master/<int:uSkaterUUID>', methods=['GET'])
 def api_master(uSkaterUUID):
     skates = base.buildMasterResponse(uSkaterUUID)
     return skates
 
+
 @app.route('/api/v1/resources/skates/active/<int:uSkaterUUID>', methods=['GET'])
 def api_active(uSkaterUUID):
     skates = base.buildActiveResponse(uSkaterUUID)
     return jsonify(skates)
+
 
 @app.route('/api/v1/resources/skates/list/<int:uSkaterUUID>', methods=['GET'])
 def api_list(uSkaterUUID):
     skates = base.buildListResponse(uSkaterUUID)
     return jsonify(skates)
 
+
+@app.route('/api/v1/resources/skates/listHours/<int:uSkaterUUID>', methods=['GET'])
+def api_listHours(uSkaterUUID):
+    listSkatesHours = base.SkatesListHoursPerConfig(uSkaterUUID)
+    return jsonify(listSkatesHours)
+
+
 @app.route('/api/v1/resources/skates/listBoots/<int:uSkaterUUID>', methods=['GET'])
 def api_listBoots(uSkaterUUID):
     skates = base.skaterListBoots(uSkaterUUID)
     return jsonify(skates)
 
+
 @app.route('/api/v1/resources/skates/listBlades/<int:uSkaterUUID>', methods=['GET'])
 def api_listBlades(uSkaterUUID):
     skates = base.skaterListBlades(uSkaterUUID)
     return jsonify(skates)
+
 
 @app.route('/api/v1/resources/skates/createSkates', methods=['POST', 'GET'])
 def createSkates():
@@ -47,17 +57,20 @@ def createSkates():
     base.addNewSkates(data)
     return str(data)
 
+
 @app.route('/api/v1/resources/skates/createBoots', methods=['POST'])
 def createBoots():
     request_data = request.get_json()
     results = base.addNewBoots(request_data)
     return results
 
+
 @app.route('/api/v1/resources/skates/createBlades', methods=['POST'])
 def createBlades():
     request_data = request.get_json()
     results = base.addNewBlades(request_data)
     return results
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5021, use_reloader=True, debug=True)
--- app/lib/baseLogic.py
+++ app/lib/baseLogic.py
@@ -1,134 +1,200 @@
-import configparser as conf
 from flask import jsonify
-import pymysql
+import st_dbConf
 
-def baseConfig():
-    configParser = conf.RawConfigParser()
-    configFilePath = r'/etc/skatetrax/settings.conf'
-    configParser.read(configFilePath)
-    appConfig = configParser.get('appKey', 'secret')
-    return appConfig
-
-def dbconnect(sql,vTUP=None):
-   configParser = conf.RawConfigParser()
-   configFilePath = r'/etc/skatetrax/settings.conf'
-   configParser.read(configFilePath)
-
-   host = configParser.get('dbconf', 'host')
-   user = configParser.get('dbconf', 'user')
-   password = configParser.get('dbconf', 'password')
-   db = configParser.get('dbconf', 'db')
-
-   con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.DictCursor, autocommit=True)
-   cur = con.cursor()
-   cur.execute(sql, vTUP)
-   tables = cur.fetchall()
-   cur.connection.commit()
-   con.close()
-   return tables
 
 def skaterListBlades(uSkaterUUID):
     q = '''
-    select fsBlades.bladesName, fsBlades.bladesModel, fsBlades.bladesSize, fsBlades.bladesPurchAmount, DATE_FORMAT(fsBlades.bladesPurchDate, "%%Y-%%m-%%d") as bladesDate, sConfig.aSkateConfigID
-    from uSkateConfig sConfig
-    INNER JOIN uSkaterBlades fsBlades ON sConfig.uSkaterUUID = fsBlades.uSkaterUUID and sConfig.uSkaterBladesID = fsBlades.bladeID
-	INNER JOIN uSkaterConfig fsConfig ON sConfig.uSkaterUUID = fsConfig.uSkaterUUID
+    SELECT
+        fsBlades.bladesName,
+        fsBlades.bladesModel,
+        fsBlades.bladesSize,
+        fsBlades.bladesPurchAmount,
+        DATE_FORMAT(fsBlades.bladesPurchDate, "%%Y-%%m-%%d") as bladesDate,
+        sConfig.aSkateConfigID
+    FROM uSkateConfig sConfig
+        INNER JOIN uSkaterBlades fsBlades
+        ON sConfig.uSkaterUUID = fsBlades.uSkaterUUID
+        AND sConfig.uSkaterBladesID = fsBlades.bladeID
+        INNER JOIN uSkaterConfig fsConfig
+        ON sConfig.uSkaterUUID = fsConfig.uSkaterUUID
     WHERE sConfig.uSkaterUUID = %s
     '''
-    results = dbconnect(q,uSkaterUUID)
+    results = st_dbConf.dbconnect(q, uSkaterUUID)
     return results
+
 
 def skaterListBoots(uSkaterUUID):
     q = '''
-    select DISTINCT fsBoots.bootsName, fsBoots.bootsModel, fsBoots.bootsSize, fsBoots.bootsPurchAmount, DATE_FORMAT(fsBoots.bootsPurchDate, "%%Y-%%m-%%d") as bootsDate
+    SELECT DISTINCT 
+        fsBoots.bootsName,
+        fsBoots.bootsModel,
+        fsBoots.bootsSize,
+        fsBoots.bootsPurchAmount,
+        DATE_FORMAT(fsBoots.bootsPurchDate, "%%Y-%%m-%%d") as bootsDate
     from uSkateConfig sConfig
-    INNER JOIN uSkaterBoots fsBoots ON sConfig.uSkaterUUID = fsBoots.uSkaterUUID and sConfig.uSkaterBootsID = fsBoots.bootID
-	INNER JOIN uSkaterConfig fsConfig ON sConfig.uSkaterUUID = fsConfig.uSkaterUUID
+        INNER JOIN uSkaterBoots fsBoots
+        ON sConfig.uSkaterUUID = fsBoots.uSkaterUUID
+        AND sConfig.uSkaterBootsID = fsBoots.bootID
+        INNER JOIN uSkaterConfig fsConfig
+        ON sConfig.uSkaterUUID = fsConfig.uSkaterUUID
     WHERE sConfig.uSkaterUUID = %s
     '''
-    results = dbconnect(q,uSkaterUUID)
+    results = st_dbConf.dbconnect(q, uSkaterUUID)
     return results
+
 
 def skaterListSkates(uSkaterUUID):
     q = '''
-    select fsBoots.bootsName, fsBoots.bootsModel, fsBlades.bladesName, fsBlades.bladesModel, sConfig.aSkateConfigID
-    from uSkateConfig sConfig
-    INNER JOIN uSkaterBoots fsBoots ON sConfig.uSkaterUUID = fsBoots.uSkaterUUID and sConfig.uSkaterBootsID = fsBoots.bootID
-    INNER JOIN uSkaterBlades fsBlades ON sConfig.uSkaterUUID = fsBlades.uSkaterUUID and sConfig.uSkaterBladesID = fsBlades.bladeID
-    where sConfig.uSkaterUUID = %s
+    select
+        fsBoots.bootsName,
+        fsBoots.bootsModel,
+        fsBlades.bladesName,
+        fsBlades.bladesModel,
+        sConfig.aSkateConfigID,
+        sum(iTime.ice_time) / 60 as aSkateConfigTime
+    from
+        uSkateConfig sConfig
+        INNER JOIN uSkaterBoots fsBoots
+        ON sConfig.uSkaterUUID = fsBoots.uSkaterUUID
+        and sConfig.uSkaterBootsID = fsBoots.bootID
+        INNER JOIN uSkaterBlades fsBlades
+        ON sConfig.uSkaterUUID = fsBlades.uSkaterUUID
+        and sConfig.uSkaterBladesID = fsBlades.bladeID
+        INNER JOIN ice_time iTime
+        ON sConfig.uSkaterUUID = iTime.uSkaterUUID
+        and sConfig.aSkateConfigID = iTime.uSkaterConfig
+    WHERE
+        sConfig.uSkaterUUID = %s
+    GROUP BY
+        sConfig.aSkateConfigID
     '''
-    results = dbconnect(q,uSkaterUUID)
+    results = st_dbConf.dbconnect(q, uSkaterUUID)
     return results
+
 
 def skaterActiveHours(uSkaterUUID):
     q = '''
-    SELECT ifnull(sum(ice_time.ice_time/60),0) as tHours from ice_time,(
-	  select sConfig.uSkaterUUID, fsBoots.bootsName, fsBoots.bootsModel, fsBlades.bladesName, fsBlades.bladesModel, sConfig.aSkateConfigID
-      from uSkateConfig sConfig
-      INNER JOIN uSkaterBoots fsBoots ON sConfig.uSkaterUUID = fsBoots.uSkaterUUID and sConfig.uSkaterBootsID = fsBoots.bootID
-      INNER JOIN uSkaterBlades fsBlades ON sConfig.uSkaterUUID = fsBlades.uSkaterUUID and sConfig.uSkaterBladesID = fsBlades.bladeID
-	  INNER JOIN uSkaterConfig fsConfig ON sConfig.uSkaterUUID = fsConfig.uSkaterUUID
-      WHERE sConfig.uSkaterUUID = %s
-      AND sConfig.aSkateConfigID = fsConfig.uSkateComboIce
-    ) actSkate
-    WHERE ice_time.uSkaterUUID = actSkate.uSkaterUUID and ice_time.uSkaterConfig = actSkate.aSkateConfigID
+    SELECT
+        ifnull(sum(ice_time.ice_time/60),0) as tHours from ice_time,(
+            SELECT
+            sConfig.uSkaterUUID,
+            fsBoots.bootsName,
+            fsBoots.bootsModel,
+            fsBlades.bladesName,
+            fsBlades.bladesModel,
+            sConfig.aSkateConfigID
+        FROM uSkateConfig sConfig
+            INNER JOIN uSkaterBoots fsBoots
+            ON sConfig.uSkaterUUID = fsBoots.uSkaterUUID
+            and sConfig.uSkaterBootsID = fsBoots.bootID
+            INNER JOIN uSkaterBlades fsBlades
+            ON sConfig.uSkaterUUID = fsBlades.uSkaterUUID
+            and sConfig.uSkaterBladesID = fsBlades.bladeID
+            INNER JOIN uSkaterConfig fsConfig
+            ON sConfig.uSkaterUUID = fsConfig.uSkaterUUID
+        WHERE sConfig.uSkaterUUID = %s
+        AND sConfig.aSkateConfigID = fsConfig.uSkateComboIce
+        ) actSkate
+    WHERE ice_time.uSkaterUUID = actSkate.uSkaterUUID
+    and ice_time.uSkaterConfig = actSkate.aSkateConfigID
     '''
-    results = dbconnect(q,uSkaterUUID)
+    results = st_dbConf.dbconnect(q, uSkaterUUID)
     return results
+
 
 def skaterActiveMeta(uSkaterUUID):
     q = '''
-    select fsBoots.bootsName, fsBoots.bootsModel, fsBlades.bladesName, fsBlades.bladesModel, sConfig.aSkateConfigID
-    from uSkateConfig sConfig
-    INNER JOIN uSkaterBoots fsBoots ON sConfig.uSkaterUUID = fsBoots.uSkaterUUID and sConfig.uSkaterBootsID = fsBoots.bootID
-    INNER JOIN uSkaterBlades fsBlades ON sConfig.uSkaterUUID = fsBlades.uSkaterUUID and sConfig.uSkaterBladesID = fsBlades.bladeID
-	INNER JOIN uSkaterConfig fsConfig ON sConfig.uSkaterUUID = fsConfig.uSkaterUUID
+    SELECT
+        fsBoots.bootsName,
+        fsBoots.bootsModel,
+        fsBlades.bladesName,
+        fsBlades.bladesModel,
+        sConfig.aSkateConfigID
+    FROM uSkateConfig sConfig
+        INNER JOIN uSkaterBoots fsBoots
+        ON sConfig.uSkaterUUID = fsBoots.uSkaterUUID
+        and sConfig.uSkaterBootsID = fsBoots.bootID
+        INNER JOIN uSkaterBlades fsBlades
+        ON sConfig.uSkaterUUID = fsBlades.uSkaterUUID
+        and sConfig.uSkaterBladesID = fsBlades.bladeID
+        INNER JOIN uSkaterConfig fsConfig
+        ON sConfig.uSkaterUUID = fsConfig.uSkaterUUID
     WHERE sConfig.uSkaterUUID = %s
     AND sConfig.aSkateConfigID = fsConfig.uSkateComboIce
     '''
-    results = dbconnect(q,uSkaterUUID)
+    results = st_dbConf.dbconnect(q, uSkaterUUID)
     return results
 
+
 def skaterListHours(uSkaterUUID):
-    ConfigID = skaterActiveHours(uSkaterUUID)
-    vTup(uSkaterUUID,ConfigID)
     q = '''
-    select fsBoots.bootsName, fsBoots.bootsModel, fsBlades.bladesName, fsBlades.bladesModel, sConfig.aSkateConfigID
+    SELECT
+        fsBoots.bootsName,
+        fsBoots.bootsModel,
+        fsBlades.bladesName,
+        fsBlades.bladesModel,
+        sConfig.aSkateConfigID
     from uSkateConfig sConfig
-    INNER JOIN uSkaterBoots fsBoots ON sConfig.uSkaterUUID = fsBoots.uSkaterUUID and sConfig.uSkaterBootsID = fsBoots.bootID
-    INNER JOIN uSkaterBlades fsBlades ON sConfig.uSkaterUUID = fsBlades.uSkaterUUID and sConfig.uSkaterBladesID = fsBlades.bladeID
-	INNER JOIN uSkaterConfig fsConfig ON sConfig.uSkaterUUID = fsConfig.uSkaterUUID
+        INNER JOIN uSkaterBoots fsBoots
+        ON sConfig.uSkaterUUID = fsBoots.uSkaterUUID
+        and sConfig.uSkaterBootsID = fsBoots.bootID
+        INNER JOIN uSkaterBlades fsBlades
+        ON sConfig.uSkaterUUID = fsBlades.uSkaterUUID
+        and sConfig.uSkaterBladesID = fsBlades.bladeID
+        INNER JOIN uSkaterConfig fsConfig
+        ON sConfig.uSkaterUUID = fsConfig.uSkaterUUID
     WHERE sConfig.uSkaterUUID = %s
     AND sConfig.aSkateConfigID = %s
     '''
-    results = dbconnect(q,uSkaterUUID)
+    results = st_dbConf.dbconnect(q, uSkaterUUID)
     return results
+
+
+def SkatesListHoursPerConfig(uSkaterUUID):
+    q = '''
+    SELECT
+        uSkaterConfig,
+        SUM(ice_time.ice_time/60) as configHours
+    FROM uSkateConfig, ice_time
+    WHERE ice_time.uSkaterUUID = %s
+    and uSkateConfig.aSkateConfigID = ice_time.uSkaterConfig
+    GROUP BY ice_time.uSkaterConfig
+    '''
+    results = st_dbConf.dbconnect(q, uSkaterUUID)
+    return results
+
 
 def buildMasterResponse(uSkaterUUID):
     skatesActive = skaterActiveMeta(uSkaterUUID)
     skatesList = skaterListSkates(uSkaterUUID)
     skatesBoots = skaterListBoots(uSkaterUUID)
     skatesBlades = skaterListBlades(uSkaterUUID)
-    skates = {'active': skatesActive, 'list': skatesList, 'skatesBoots': skatesBoots, 'skatesBlades': skatesBlades}
+    skates = {'active': skatesActive, 'list': skatesList,
+              'skatesBoots': skatesBoots, 'skatesBlades': skatesBlades}
     return jsonify(skates)
+
 
 def buildActiveResponse(uSkaterUUID):
     skatesActive = skaterActiveMeta(uSkaterUUID)
     return skatesActive
 
+
 def buildListResponse(uSkaterUUID):
     skatesList = skaterListSkates(uSkaterUUID)
     return skatesList
 
+
 def buildMasterResponseTest(uSkaterUUID):
-    #skatesActive = skaterActiveMeta(uSkaterUUID)
-    #skatesList = skaterListSkates(uSkaterUUID)
-    #skatesBoots = skaterListBoots(uSkaterUUID)
-    #skatesBlades = skaterListBlades(uSkaterUUID)
-    #skates = {'active': skatesActive, 'list': skatesList, 'skatesBoots': skatesBoots, 'skatesBlades': skatesBlades}
+    # skatesActive = skaterActiveMeta(uSkaterUUID)
+    # skatesList = skaterListSkates(uSkaterUUID)
+    # skatesBoots = skaterListBoots(uSkaterUUID)
+    # skatesBlades = skaterListBlades(uSkaterUUID)
+    # skates = {'active': skatesActive, 'list': skatesList,
+    #           'skatesBoots': skatesBoots, 'skatesBlades': skatesBlades}
     skatesActive = skaterActiveMeta(uSkaterUUID)
-    print(jsonify({'active':skatesActive}))
-    return jsonify({'active':skatesActive})
+    print(jsonify({'active': skatesActive}))
+    return jsonify({'active': skatesActive})
+
 
 def addNewBoots(request_data):
     uSkaterUUID = request_data['uSkaterUUID']
@@ -158,14 +224,29 @@ def addNewBoots(request_data):
     else:
         bootsPurchAmount = request_data['bootsPurchAmount']
 
-    bootsTuple = (bootsName, bootsModel, bootsSize, bootsPurchDate, bootsPurchAmount, uSkaterUUID)
-    bootsQuery = "INSERT INTO uSkaterBoots (bootsName, bootsModel, bootsSize, bootsPurchDate, bootsPurchAmount, uSkaterUUID, bootID) select %s, %s, %s, %s, %s, %s, max(bootID)+1 from uSkaterBoots;"
-    results = dbconnect(bootsQuery,bootsTuple)
+    bootsTuple = (bootsName, bootsModel, bootsSize,
+                  bootsPurchDate, bootsPurchAmount, uSkaterUUID)
+    bootsQuery = '''
+    INSERT INTO uSkaterBoots (
+        bootsName,
+        bootsModel,
+        bootsSize,
+        bootsPurchDate,
+        bootsPurchAmount,
+        uSkaterUUID,
+        bootID
+        )
+    SELECT %s, %s, %s, %s, %s, %s, max(bootID)+1 from uSkaterBoots;
+    '''
+    # results = st_dbConf.dbconnect(bootsQuery, bootsTuple)
 
     # nice diagnostic check here
-    #results = '''{} {} boots, size {}, bought on {} for ${}'''.format(bootsName, bootsModel, bootsSize, bootsPurchDate, bootsPurchAmount)
-    #print(results)
+    #
+    # results = '''{} {} boots, size {}, bought on {} for ${}'''.format(
+    #    bootsName, bootsModel, bootsSize, bootsPurchDate, bootsPurchAmount)
+    # print(results)
     return str(200)
+
 
 def addNewBlades(request_data):
     uSkaterUUID = request_data['uSkaterUUID']
@@ -195,13 +276,27 @@ def addNewBlades(request_data):
     else:
         bladesPurchAmount = request_data['bladesPurchAmount']
 
-    bladesTuple = (bladesName, bladesModel, bladesSize, bladesPurchDate, bladesPurchAmount, uSkaterUUID)
-    bladesQuery = "INSERT INTO uSkaterBlades (bladesName, bladesModel, bladesSize, bladesPurchDate, bladesPurchAmount, uSkaterUUID, bladeID) select %s, %s, %s, %s, %s, %s, max(bladeID)+1 from uSkaterBlades;"
-    results = dbconnect(bladesQuery,bladesTuple)
+    bladesTuple = (bladesName, bladesModel, bladesSize,
+                   bladesPurchDate, bladesPurchAmount, uSkaterUUID)
+    bladesQuery = '''
+    INSERT INTO uSkaterBlades (
+        bladesName,
+        bladesModel,
+        bladesSize,
+        bladesPurchDate,
+        bladesPurchAmount,
+        uSkaterUUID,
+        bladeID
+        )
+    SELECT %s, %s, %s, %s, %s, %s, max(bladeID)+1
+    FROM uSkaterBlades;
+    '''
+    results = st_dbConf.dbconnect(bladesQuery, bladesTuple)
     return str(200)
+
 
 def addNewSkates(request_data):
     print(request_data)
     skatesQuery = "INSERT INTO uSkateConfig (uSkaterUUID, uSkaterBladesID, uSkaterBootsID, sType, sActive, aSkateConfigID) select %s, %s, %s, 1, 1, max(aSkateConfigID)+1 from uSkateConfig;"
-    results = dbconnect(skatesQuery,request_data)
+    results = st_dbConf.dbconnect(skatesQuery, request_data)
     return str(200)
